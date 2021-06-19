69ad1466a1fa9d403d42df0e2723bafea727f8cd
san-smith <san-smith@mail.ru>
2021-06-19 18:04:13 +0700

    Add dark theme

--- lib/internal/application.dart
+++ lib/internal/application.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:map_generator/presentation/home/home_screen.dart';
+import 'package:map_generator/presentation/ui/theme.dart';
 
 class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MapGenerator',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: darkTheme,
       home: Home(),
     );
   }
